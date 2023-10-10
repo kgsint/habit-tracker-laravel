53244120ba484bbd1619475d9931e235a02cdb3c
Kaung Sint <kaungsintkoko99@gmail.com>
2023-10-10 15:14:19 +0630

    force url to load https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url): void
     {
-
+        $url->forceScheme('https');
     }
 }
