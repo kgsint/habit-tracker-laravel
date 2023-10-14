72334c3fe847587095677d96552f6091a1abb145
Kaung Sint <kaungsintkoko99@gmail.com>
2023-10-14 23:31:55 +0630

    fix issue when a task was updated

--- resources/js/task-update.js
+++ resources/js/task-update.js
@@ -1,18 +1,39 @@
 import axios from "axios"
 
+// check or uncheck task
 document.querySelectorAll('#task-checkbox').forEach(checkbox => {
     checkbox.addEventListener('change', e => {
-        const form = e.currentTarget.closest('form')
-        const formData = new FormData(form)
-
-        const data = Object.fromEntries(formData.entries())
-
-        // form has @method('PATCH) => _method="PATCH"  so it's typically patch request,
-        axios.post(form.action, data).then(res => {
-            if(res.status === 200) {
-                location.reload()
-            }
-        })
+        // update task
+        updateTask(e)
 
     })
 })
+
+// when hit enter from task input, Update
+document.querySelectorAll('#taskUpdateForm input[type="text"]').forEach(input => {
+    input.addEventListener('keydown', e => {
+        // when press enter
+        if(e.key === 'Enter') {
+            // prevent reload when the user hit enter
+            e.preventDefault()
+            // update task
+            updateTask(e)
+        }
+
+    })
+})
+
+// reusable utils function
+function updateTask(e) {
+    const form = e.currentTarget.closest('form')
+    const formData = new FormData(form)
+
+    const data = Object.fromEntries(formData.entries())
+
+    // form has @method('PATCH) => _method="PATCH"  so it's typically patch request,
+    axios.post(form.action, data).then(res => {
+        if(res.status === 200) {
+            location.reload()
+        }
+    })
+}
--- resources/views/components/tasks/input.blade.php
+++ resources/views/components/tasks/input.blade.php
@@ -5,6 +5,7 @@
     class="w-full outline-none border-transparent border-b border-b-gray-200 focus-visible:border-b-2 focus-visible:border-blue-50
     dark:text-white dark:bg-black {{ $completed ? 'line-through text-gray-500' : '' }}"
     value="{{ $body }}"
+    id="task-input"
     {!! $attributes !!}
     {{ $disabled ? 'disabled' : '' }}
 >
